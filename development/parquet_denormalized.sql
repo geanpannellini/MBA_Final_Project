5255e94cc42d0ba75f909e17eb236f9e3234448b
Gean Pannellini <70926945+geanpannellini@users.noreply.github.com>
2024-05-16 10:21:22 -0300

    Create parquet_denormalized.sql

---
+++ development/parquet_denormalized.sql
@@ -0,0 +1,15 @@
+-- SEM NORMALIZACAO
+EXPLAIN ANALYZE
+SELECT
+    scrape_id,
+    COUNT(DISTINCT id) AS contagem_listings,
+    COUNT(DISTINCT picture_url) AS contagem_fotos_url,
+    COUNT(DISTINCT host_name) AS contagem_unicas_de_nomes_de_host,
+    COUNT(DISTINCT neighbourhood) AS contagem_bairros_unicos,
+    AVG(review_scores_rating) AS average_review_scores_rating
+
+FROM read_parquet('data-airbnb/pqt_bronze_listings.parquet')
+
+WHERE scrape_id IN ('20190313042552','20180414160018')
+GROUP BY scrape_id
+ORDER BY scrape_id;
